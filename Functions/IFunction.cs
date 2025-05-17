f272d41e590281fc0438a6648a1f5802fec266ea
Mateusz Guzek <mateusz.guzek@protonmail.com>
2025-05-18 01:10:55 +0200

    xd

---
+++ Functions/IFunction.cs
@@ -0,0 +1,16 @@
+﻿using Numerics.NET;
+using Interval = Intervals.Interval;
+
+namespace Functions;
+
+public interface IFunction
+{
+    public BigFloat Eval(BigFloat x);
+    
+    public Interval Eval(Interval x);
+
+    public String StringRepresentation
+    {
+        get;
+    }
+}
---
+++ UserFunctions/MyFunctions.cs
@@ -0,0 +1,23 @@
+﻿using Functions;
+using Numerics.NET;
+using Interval = Intervals.Interval;
+
+namespace UserFunctions;
+
+public class MyFunctions : IMyFunctions
+{
+    public IEnumerable<IFunction> Functions()
+    {
+        return new List<IFunction>()
+        {
+            new UserFunction("x^2", 
+                x => BigFloat.Pow(x,2) - 1,
+                x => x.Sqr() - new Interval(1)
+                ),
+            new UserFunction("sin(x)", 
+                x => BigFloat.Sin(x),
+                x => x.Sin()
+            )
+        };
+    }
+}
