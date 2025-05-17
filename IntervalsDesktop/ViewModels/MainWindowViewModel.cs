9f556c49798fbdc3f1c4ec34cba572a1e4d32608
Mateusz Guzek <mateusz.guzek@protonmail.com>
2025-05-18 01:10:21 +0200

    czytanie dll przy file pickerem

--- IntervalsDesktop/ViewModels/MainWindowViewModel.cs
+++ IntervalsDesktop/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,11 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using Avalonia.Controls;
+using Avalonia.Platform.Storage;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Nonlinear_Solvers;
+using Numerics.NET;
 
 namespace IntervalsDesktop.ViewModels;
 
@@ -16,14 +23,43 @@ public partial class MainWindowViewModel : ViewModelBase
         "Przedziałowa"
     };
 
-    private string _selectedMode;
-
+    private string _selectedMode = "Standardowa";
+    
     public string SelectedMode
     {
         get => _selectedMode;
-        set {
-        _selectedMode = value;
+        set
+        {
+            IsArithmeticModeSelected = value == "Przedziałowa";
+            _selectedMode = value;
+        }
     }
-}
+    
+    public List<string> Methods { get; } = new()
+    {
+        "Metoda połowienia",
+        "Regula Falsi",
+        "Metoda siecznych"
+    };
 
+    [ObservableProperty] private string _selectedMethod = "Metoda połowienia";
+
+    [ObservableProperty] private string _a1 = "";
+    [ObservableProperty] private string _a2 = "";
+    [ObservableProperty] private string _b1 = "";
+    [ObservableProperty] private string _b2 = "";
+    
+    
+    [ObservableProperty] private Result<BigFloat> _resultBF;
+    [ObservableProperty] private Result<Interval> _resultINT;
+    
+    
+
+
+    [ObservableProperty] private bool _isArithmeticModeSelected;
+
+
+
+
+    
 }
--- IntervalsDesktop/Views/MainWindow.axaml.cs
+++ IntervalsDesktop/Views/MainWindow.axaml.cs
@@ -1,4 +1,11 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Platform.Storage;
+using Functions;
 
 namespace IntervalsDesktop.Views;
 
@@ -7,5 +14,44 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    public async void OnSelectLibraryButtonClicked(object? sender, RoutedEventArgs e)
+    {
+        // Get top level from the current control. Alternatively, you can use Window reference instead.
+        var topLevel = TopLevel.GetTopLevel(this);
+
+        // Start async operation to open the dialog.
+        var files = await topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+        {
+            Title = "Otwórz swoją bibliotekę",
+            AllowMultiple = false
+        });
+
+        if (files.Count >= 1)
+        {
+            string pathToDll = files[0].Path.AbsolutePath;
+
+            Assembly assembly = Assembly.LoadFrom(pathToDll);
+
+            // Znajdź typ implementujący IMyFunctions
+            Type? myFunctionsType = assembly
+                .GetTypes()
+                .FirstOrDefault(t => typeof(IMyFunctions).IsAssignableFrom(t) && !t.IsInterface && !t.IsAbstract);
+
+            if (myFunctionsType != null)
+            {
+                var instance = Activator.CreateInstance(myFunctionsType) as IMyFunctions;
+
+                if (instance != null)
+                {
+                    var functions = instance.Functions();
+                    foreach (var func in functions)
+                    {
+                        Console.WriteLine(func.StringRepresentation);
+                    }
+                }
+            }
+        }
     }
 }
